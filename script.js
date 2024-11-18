3e579caa1b62857eb6a9f024c2e14660427a3229
treblig-punisher <gilbertplayx@hotmail.com>
2024-11-18 16:35:50 -0500

    added background, and filters, and new functionality

--- script.js
+++ script.js
@@ -15,7 +15,16 @@ const userInfoWrapper = document.querySelector("#user-info-wrapper")
 const signOutButton = document.querySelector('#sign-out-button')
 const signInForm = document.querySelector('#log-in-form')
 const signUpForm = document.querySelector('#sign-up-form')
+const companyNameInput = document.querySelector('#company-name')
+const companyNameLabel = document.querySelector('#company-name-label')
+const noChoiceHasCompany = document.querySelector('#no-choice')
+const bookTripButton = document.querySelector('#call-to-action button:first-of-type')
+const joinUsButton = document.querySelector('#call-to-action button:last-of-type')
+const yesChoiceHasCompany = document.querySelector('#yes-choice')
+let companyTextBoxEnabled = false
+const forms = document.querySelectorAll('form') 
 const body = document.body
+
 const footer = `
     <footer>
       <div> © ${new Date().getFullYear()} Copyright: <span>Software Solution <strong>SRL</strong></span> <span>“Soluciones con un solo clic”</span></div>
@@ -28,13 +37,24 @@ checkInNavButton.addEventListener('click', (e)=> showPopOverModal(e, checkInPopO
 bookFlightNavButton.addEventListener('click', (e)=> showPopOverModal(e, bookFlightPopOver))
 signInForm.addEventListener('submit', validateSignIn)
 signUpForm.addEventListener('submit', validateSignUp)
-
+bookTripButton.addEventListener('click', ()=>bookFlightNavButton.click())
+joinUsButton.addEventListener('click', ()=> signUpButton.click())
+noChoiceHasCompany.addEventListener('click',()=>{
+    companyTextBoxEnabled = false
+    toggleCompanyTextBox(companyTextBoxEnabled)    
+})
+yesChoiceHasCompany.addEventListener('click',()=>{
+    companyTextBoxEnabled = true
+    toggleCompanyTextBox(companyTextBoxEnabled)
+})
 let userLoggedIn = false
 
 const signedInMenu = document.querySelector('#signed-in-menu')
 window.addEventListener('load', ()=>{
+    toggleCompanyTextBox(companyTextBoxEnabled, false)
     uiUpdateOnSignIn()
-    updateUserName()
+    updateUserName()    
+    forms.forEach(form=> form.reset())
 })
 signOutButton.addEventListener('click', userLogOut)
 function print(msg){
@@ -77,7 +97,10 @@ closePopOverButtons.forEach(button =>{
 })
 function hidePopOver(){
     const popOvers = document.querySelectorAll('[popover]') 
+    companyTextBoxEnabled = false
+    forms.forEach(form=> form.reset())
     popOvers.forEach(popover => popover.close())
+    toggleCompanyTextBox(companyTextBoxEnabled, false)
 }
 
 function userLogOut(){
@@ -85,6 +108,7 @@ function userLogOut(){
     userLoggedIn = false 
     signedInMenu.style.display = !userLoggedIn? 'none': 'flex'
     toggleSignInSignUpOptions(userLoggedIn)
+    joinUsButton.style.display = 'initial'
     localStorage.setItem('isUserSignedIn', userLoggedIn)
     // uiUpdateOnSignIn()
 }
@@ -97,6 +121,7 @@ function validateSignIn(e){
     if(userDetails !== 'undefined'){
         const spanElement = document.querySelector(`#signed-in-menu span:first-of-type`)
         spanElement.textContent= `Welcome, ${userDetails.firstName}`
+        joinUsButton.style.display = 'none'
         const userJson = JSON.stringify(userDetails)
         localStorage.setItem('userDetails', userJson)
     }
@@ -112,15 +137,17 @@ function validateSignUp(e){
     const userCreatedPw = document.querySelector('#create-user-password')
     const userConfirmPw = document.querySelector('#confirm-password')
     const userAddress = document.querySelector('#user-address')
-    const userCompanyName = document.querySelector('#company-name')
+
+    const userCompanyName = companyTextBoxEnabled? document.querySelector('#company-name'): 'undefined'
     const userPhoneNum = document.querySelector('#phone-number')
     if(userCreatedPw.value !== userConfirmPw.value){
         console.log("password must match")
         return
     }
+    const hasCompany = userCompanyName !== 'undefined'? userCompanyName.value: ""
     const newClient = {
         firstName: userName.value, lastName: userLastName.value, email: userEmail.value, password: userCreatedPw.value,
-        address: userAddress.value, companyName: userCompanyName.value, phoneNumber: userPhoneNum.value, milesAccumulated: 0
+        address: userAddress.value, companyName: hasCompany, phoneNumber: userPhoneNum.value, milesAccumulated: 0
     }
     localStorage.setItem('userDetails', JSON.stringify(newClient))
     localStorage.setItem('isUserSignedIn', true)
@@ -128,8 +155,6 @@ function validateSignUp(e){
     createNewClient(newClient)
     signUpPopOver.close()
     uiUpdateOnSignIn()
-
-
 }
 
 function updateUserName(){
@@ -137,4 +162,30 @@ function updateUserName(){
     const userDetails = localStorage.getItem('userDetails')? JSON.parse(localStorage.getItem('userDetails')): 'undefined'
     if(userDetails === 'undefined') return
     spanElement.textContent= `Welcome, ${userDetails.firstName}`    
+}
+
+function toggleCompanyTextBox(textBoxEnabled, removeChecked = true){
+    if(textBoxEnabled) companyNameInput.setAttribute('required',"required")
+    else companyNameInput.removeAttribute('required')
+    if(removeChecked)noChoiceHasCompany.removeAttribute('checked')
+    else noChoiceHasCompany.setAttribute('checked', 'checked')
+    const value = textBoxEnabled? 'flex': 'none'
+    companyNameInput.style.display = value
+    companyNameLabel.style.display = value
+}
+
+function generateFlight(flight){
+    const newFlight = 
+    ` 
+    <div class="flight-template">
+        <div>
+            <span>${flight.date}</span>
+            <span>${flight.landingDate}</span>
+        </div>
+        <div>${flight.seatType}</div>
+        <div>${flight.cost}</div>
+    </div>
+    `
+    const flightsContainer = document.querySelector('#available-flights-container')
+    flightsContainer.insertAdjacentHTML('beforeend', newFlight)
 }
